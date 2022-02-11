f826e69ec47ec73d13cc0d43ab226ec5cece001c
Mark Kulik <kulik_mark@mail.ru>
2022-02-11 17:10:29 +0300

    Finishing the App. (DesignCode Course)

--- WeatherSwiftUI/Views/ContentView.swift
+++ WeatherSwiftUI/Views/ContentView.swift
@@ -9,23 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var locationManager = LocationManager()
+    var weatherManager = WeatherManager()
+    @State var weather: ResponseBody?
     
     var body: some View {
         VStack {
-            
             if let location = locationManager.location {
-                Text("Your coordinates are: Latitude:\(location.latitude), Longitude:\(location.longitude)")
+                if let weather = weather {
+                    WeatherView(weather: weather)
                 } else {
-                    if locationManager.isLoading {
                         LoadingView()
+                        .task {
+                            do {
+                               weather = try await weatherManager.getCurrentWeather(latitude: location.latitude, longitude: location.longitude)
+                            } catch {
+                                print("Error getting weather: \(error)")
+                            }
+                        }
+                }
                     } else {
-                        WelcomeView()
-                            .environmentObject(locationManager)
+                        if locationManager.isLoading {
+                            LoadingView()
+                        } else {
+                            WelcomeView()
+                                .environmentObject(locationManager)
+                        }
                     }
                 }
-        }
-        .background(Color(hue: 0.660, saturation: 1.0, brightness: 0.489))
-        .preferredColorScheme(.dark)
+                .background(Color(hue: 0.591, saturation: 1.0, brightness: 0.606))
+                .preferredColorScheme(.dark)
     }
 }
 
---
+++ WeatherSwiftUI/Views/WeatherView.swift
@@ -0,0 +1,105 @@
+//
+//  WeatherView.swift
+//  WeatherSwiftUI
+//
+//  Created by Марк Кулик on 10.02.2022.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    
+    var weather: ResponseBody
+    
+    var body: some View {
+        ZStack(alignment: .leading) {
+            VStack {
+                
+                VStack (alignment: .leading, spacing: 5) {
+                    Text(weather.name)
+                        .bold().font(.title)
+                    Text ("Today, \(Date().formatted(.dateTime.month().day().hour().minute()))")
+                        .fontWeight(.light)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                
+                Spacer()
+                
+                VStack {
+                    HStack {
+                        VStack(spacing: 20) {
+                            Image(systemName: "sun.max")
+                                .font(.system(size: 40))
+                            Text (weather.weather[0].main)
+                        }
+                        .frame(width: 150, alignment: .leading)
+                        .padding()
+                        
+                        Spacer()
+                        
+                        Text(weather.main.feelsLike.roundDouble() + "°")
+                            .font(.system(size: 100))
+                            .fontWeight(.bold)
+                            .padding()
+                        
+                    }
+                    
+                    Spacer()
+                        .frame(height: 80)
+                    
+                    Image("newyork")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 420)
+                    
+                    Spacer()
+                    
+                }
+                .frame(maxWidth: .infinity)
+                
+            }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            VStack {
+                Spacer()
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("Weather now")
+                        .bold()
+                        .padding(.leading, 20)
+                        .padding(.bottom, 10)
+                    
+                    HStack {
+                        WeatherRow(logo: "thermometer", name: "Min temp", value: (weather.main.tempMin.roundDouble() + ("°")))
+                        Spacer()
+                        WeatherRow(logo: "thermometer", name: "Max temp", value: (weather.main.tempMax.roundDouble() + "°"))
+                    }
+                    .padding()
+
+                    HStack {
+                        WeatherRow(logo: "wind", name: "Wind speed", value: (weather.wind.speed.roundDouble() + " m/s"))
+                        Spacer()
+                        WeatherRow(logo: "humidity", name: "Humidity", value: "\(weather.main.humidity.roundDouble())%")
+                    }
+                    .padding()
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                .padding(.bottom, 20)
+                .foregroundColor(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
+                .background(.white)
+                .cornerRadius(40, corners: [.topRight, .topLeft])
+            }
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .background(Color(hue: 0.591, saturation: 1.0, brightness: 0.606))
+        .preferredColorScheme(.dark)
+    }
+}
+
+struct WeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherView(weather: previewWeather)
+    }
+}
